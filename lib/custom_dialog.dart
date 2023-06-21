3d72c7310c4389f94bbdc9552307d7512ac47aae
unknown <tejaspatange5678@gmail.com>
2023-06-21 15:58:37 +0530

    Initial commit

---
+++ lib/custom_dialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  final title;
+  final content;
+  final VoidCallback callback;
+  final actionText;
+
+  CustomDialog(this.title, this.content, this.callback,
+      [this.actionText = "Reset"]);
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: new Text(title),
+      content: new Text(content),
+      actions: <Widget>[
+        TextButton(onPressed: callback,
+            style: ButtonStyle(
+              backgroundColor:MaterialStateProperty.all(Colors.white),
+            ),
+            child:
+        Text(actionText)
+        )
+
+      ],
+    );
+  }
+}
